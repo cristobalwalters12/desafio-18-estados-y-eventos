44cab952f183d3c1e36286a3f74312277125585f
cristobalwalters12 <cristobalwalters@gmail.com>
2023-09-28 01:01:13 -0300

    cambios minimos

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,7 +1,6 @@
 import React, { useState } from "react";
 import { Form, FormControl, FormGroup, Button } from "react-bootstrap";
-import Alert from "./Alert"; // Asegúrate de poner la ruta correcta
-
+import Alert from "./Alert";
 function Formulario() {
   const [nombre, setNombre] = useState("");
   const [email, setEmail] = useState("");
@@ -18,7 +17,7 @@ function Formulario() {
       password === "" ||
       confirmpassword === ""
     ) {
-      setAlertMessage("Todos los campos son obligatorios");
+      setAlertMessage("Todos los campos son obligatorios!");
       setAlertVariant("danger");
     } else {
       setAlertMessage("Registro exitoso");
@@ -30,33 +29,33 @@ function Formulario() {
       <FormGroup>
         <FormControl
           type="text"
-          placeholder="Enter name"
+          placeholder="Nombre"
           onChange={(e) => setNombre(e.target.value)}
           value={nombre}
           className="mb-3 mt-3"
         />
         <FormControl
           type="email"
-          placeholder="Enter email"
+          placeholder="Tuemail@ejemplo.com"
           onChange={(e) => setEmail(e.target.value)}
           value={email}
           className="mb-3"
         />
         <FormControl
           type="password"
-          placeholder="Enter password"
+          placeholder="Contraseña"
           onChange={(e) => setPassword(e.target.value)}
           value={password}
           className="mb-3"
         />
         <FormControl
           type="password"
-          placeholder="Confirm password"
+          placeholder="Confirma de contraseña"
           onChange={(e) => setConfirmPassword(e.target.value)}
           value={confirmpassword}
         />
         <Button type="submit" className="mt-3 w-100" variant="success">
-          Registrar
+          Registrarse
         </Button>
         {alertMessage && <Alert text={alertMessage} variant={alertVariant} />}
       </FormGroup>
